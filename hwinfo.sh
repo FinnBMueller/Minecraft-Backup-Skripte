4d8bf93cb86a253e193c7c5ab6718f7e81ab6b0a
FinnBMueller <137657539+FinnBMueller@users.noreply.github.com>
2026-02-22 05:32:09 +0100

    hwinfo.sh

--- hwinfo.sh
+++ hwinfo.sh
@@ -1,32 +1,4 @@
-#!/bin/sh
-
-#################################
-# Konfiguration
-#################################
-
-LOCKFILE="/var/lock/minecraft-shutdown-now.lock"
-
-#################################
-# Lockfile (Race-Condition-Schutz)
-#################################
-
-if [ -f "$LOCKFILE" ]; then
-    echo "[Backup] ❌ Backup läuft bereits - Abbruch."
-    exit 1
-fi
-
-touch "$LOCKFILE"
-
-cleanup() {
-    rm -f "$LOCKFILE"
-}
-trap cleanup EXIT
-
-echo "[Backup] 🔒 Lock gesetzt"
-
-#################################
-# Server aktion
-#################################
+#!/bin/bash
 
 #starts a textbased hardware monitoring gui
 htop
